a38853df070e3c22fefd289227d27b9f1fbf069c
Vaibhav Garg <gvaibhav223@gmail.com>
2022-11-21 13:19:26 +0530

    learning module import and export function adn operation function and file name directory name functions

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-function SayHello() {
-  console.log("Helo");
-}
+// let loger = require("./logger");
+// function Logging() {
+//   loger.LastName("Vaibhav");
+// }
 
-SayHello();
+// Logging();
+// console.log( __filename);
+// console.log(__dirname);
+// const os = require("os");
+
+// const totaMemory = os.totalmem();
+// const freeMemory = os.freemem();
+// const restmemory = totaMemory - freeMemory;
+// console.log(
+//   `the total memoy is ${totaMemory} and free Memory is ${freeMemory}`
+// );
+// console.log(restmemory / 100);
---
+++ logger.js
@@ -0,0 +1,6 @@
+let myMessage = "This is last mesage by vaiobhav";
+function Loger(message) {
+  console.log(`This is ${message} Last mnessage to everyone from everywhere`);
+}
+module.exports.LastName = Loger;
+module.exports.my_Message = myMessage;
